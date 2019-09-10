6c17c1aa21bf33311793458d125d2314bb4f9367
1firework <859798281@qq.com>
2019-09-10 10:22:57 +0800

    第一次作业

---
+++ jiou.java
@@ -0,0 +1,10 @@
+public class jiou{
+      public static void main(String[] arge) {
+           int num=20;
+           if(num%2==0){
+             System.out.println("num是一个偶数");
+           } else {
+               System.out.println("num是一个奇数");
+       }  
+  }
+}
